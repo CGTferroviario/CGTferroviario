be2aa6b0e480624d0eb41645219b99f7a31c3b0a
Peibol <informaticacgtferroviario@gmail.com>
2022-01-12 17:51:07 +0100

    Empezando
    Abrimos proyecto con el index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<HTML lang="es-ES" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">	
+	<HEAD>
+		<TITLE>SECTOR FEDERAL FERROVIARIO - CGT</TITLE>
+	</HEAD>
+	<BODY>
+		
+	</BODY>
+</HTML>
