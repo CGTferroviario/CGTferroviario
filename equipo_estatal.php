33c2e6d67f7a1d759ebbdb4a8a2dc554c5d2ba19
Peibol <informaticacgtferroviario@gmail.com>
2022-03-16 16:55:11 +0100

    Actualización del equipo estatal

---
+++ equipo_estatal.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<HTML lang="es-ES" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<HEAD>
+  <TITLE>SECTOR FEDERAL FERROVIARIO - CGT</TITLE>
+		<META charset="UTF-8">
+  <META name="copyright" content="SFFCGT">
+  <META name="author" content="Chema Santamar&iacute;a Garc&iacute;a">
+  <META name="description" content="Sector Federal Ferroviario - CGT">
+<meta property="og:description" content="El Sector Federal Ferroviario">
+<meta property="og:image" content="http://www.sff-cgt.org/imagenes/comunicado.jpg">
+  <META name="keywords" content="sindicato sector federal ferroviario renfe adif anarcosindicalismo trabajadores derechos">
+  <link rel="shortcut icon" href="http://www.sff-cgt.org/favicon.ico">
+  <link rel="STYLESHEET" type="text/css" href="estilos_sff_cgt.css">
+  <link rel="image_src" href="http://www.sff-cgt.org/imagenes/comunicado.jpg">
+<script type="text/javascript" language="JavaScript1.2" src="menus/stmenu.js"></script>
+<script type="text/javascript" language="JavaScript1.2" src="menus/funciones.js"></script>
+<meta name="google-translate-customization" content="122c4f0e408b47da-7963a17585edc02d-g6801b4587076e793-f"></meta>
+</HEAD>
+
+<BODY>
+
+<TABLE align=center border=0 cellpadding=6 cellspacing=1 bgcolor=ffffff>
+    <TR valign=top>
+      <TD colspan=3><?php include "cabecera.php"; ?></TD>
+    </TR>
+    <TR valign=top>
+      <TD>
+
+
+<DIV class='titulo_seccion'>Equipo estatal del SECTOR FEDERAL FERROVIARIO - CGT</DIV><DIV class='separador18'>&nbsp;</DIV>
+
+<TABLE width=95% align=center border=0>
+  <TR>
+     <TD colspan=3 bgcolor=955E55><font color=white size=4><b>&nbsp;&nbsp;&nbsp; Secretariado Permanente SECTOR FEDERAL FERROVIARIO</b></font></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a General</TD><TD>Noelia Mart&iacute;n Sanguino</TD><TD><A HREF="mailto:s.general@cgtferroviario.es">s.general@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a Organizaci&oacute;n</TD><TD>Daniel Hidalgo Mu&ntilde;oz</TD><TD><A HREF="mailto:s.organizacion@cgtferroviario.es">s.organizacion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Acci&oacute;n Sindical y Salud Laboral</TD><TD>Jose Manuel Mu&ntilde;oz P&oacute;liz</TD><TD><A HREF="mailto:accionsindical@cgtferroviario.es">accionsindical@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Adm&oacute;n, Finanzas, Medios Sindicales</TD><TD>Ignacio Barros Alonso</TD><TD><A HREF="mailto:finanzas@cgtferroviario.es">finanzas@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Formaci&oacute;n</TD><TD>Jes&uacute;s Bermejo Bermejo</TD><TD><A HREF="mailto:formacion@cgtferroviario.es">formacion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Expansi&oacute;n</TD><TD>Amelia Penedo Fern&aacute;ndez</TD><TD><A HREF="mailto:expansion@cgtferroviario.es">expansion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Comunicaci&oacute;n</TD><TD>Javier Argerich P&eacute;rez</TD><TD><A HREF="mailto:comunicacion@cgtferroviario.es">comunicacion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Jur&iacute;dica</TD><TD>Jos&eacute; Dolz Cabo</TD><TD><A HREF="mailto:juridica@cgtferroviario.es">juridica@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Acci&oacute;n Social y Mujer</TD><TD>Elena Garc&iacute;a Esparza</TD><TD><A HREF="mailto:accionsocial@cgtferroviario.es">accionsocial@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>Secretar&iacute;a de Expansi&oacute;n de las Empresas Privadas</TD><TD>Diana Murciano Paredes</TD><TD><A HREF="mailto:serviciosauxiliares@cgtferroviario.es">serviciosauxiliares@cgtferroviario.es</A></TD>
+  </TR>
+
+  <TR><TD colspan=3><HR></TD></TR>
+  
+  <TR>
+     <TD colspan=3 bgcolor=955E55><font color=white size=4><b>&nbsp;&nbsp;&nbsp; Responsables empresas SECTOR FEDERAL FERROVIARIO</b></font></TD>
+  </TR>
+  <TR>
+     <TD>ADIF</TD><TD>Enrique Villase&ntilde;or Osuna</TD><TD><A HREF="mailto:adif@cgtferroviario.es">adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>RENFE OPERADORA</TD><TD>Pablo Rodr&iacute;guez Villanueva</TD><TD><A HREF="mailto:renfe@cgtferroviario.es">renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>FERROVIAL</TD><TD>Diana Murciano Paredes</TD><TD><A HREF="ferrovial@cgtferroviario.es">ferrovial@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>LOGIRAIL</TD><TD>Nerea Gonz&aacute;lez Guti&eacute;rrez</TD><TD><A HREF="logirail@cgtferroviario.es">logirail@cgtferroviario.es</A></TD>
+  </TR>
+ 
+  <TR><TD colspan=3><HR></TD></TR>
+
+  <TR>
+     <TD colspan=3 bgcolor=955E55><font color=white size=4><b>&nbsp;&nbsp;&nbsp; COORDINACI&Oacute;N TERRITORIAL Equipo Estatal</b></font></TD>
+  </TR>
+  <TR>
+     <TD>MADRID</TD><TD>Enrique Fern&aacute;ndez / Marco Coppola</TD><TD><A HREF="mailto:ct.madrid@cgtferroviario.es">ct.madrid@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>ANDALUC&Iacute;A</TD><TD></TD><TD><A HREF="mailto:ct.andalucia@cgtferroviario.es">ct.andalucia@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>PA&Iacute;S VALEN&Ccedil;IA Y MURCIA</TD><TD></TD><TD><A HREF="mailto:ct.paisvalencia@cgtferroviario.es">ct.paisvalencia@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>CATALUNYA</TD><TD>Enrique Villase&ntilde;or Osuna</TD><TD><A HREF="mailto:ct.catalunya@cgtferroviario.es">ct.catalunya@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>ARAG&Oacute;N / LA RIOJA</TD><TD>Jose Luis Fern&aacute;ndez Jim&eacute;nez</TD><TD><A HREF="mailto:ct.aragon@cgtferroviario.es">ct.aragon@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>EUSKADI / NAVARRA</TD><TD>Iker Izquierdo Barreiro</TD><TD><A HREF="mailto:ct.euskadi@cgtferroviario.es">ct.euskadi@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>C-LE&Oacute;N</TD><TD>Felix Huete Gonz&aacute;lez</TD><TD><A HREF="mailto:ct.cyl@cgtferroviario.es">ct.cyl@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>CANTABRIA</TD><TD>Javier Cobo Gonz&aacute;lez</TD><TD><A HREF="mailto:ct.cantabria@cgtferroviario.es">ct.cantabria@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>ASTURIAS</TD><TD>Diego S&aacute;nchez &Aacute;lvarez</TD><TD><A HREF="mailto:ct.asturias@cgtferroviario.es">ct.asturias@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>GALICIA</TD><TD>&Aacute;ngel Valladares Nocito</TD><TD><A HREF="mailto:ct.galicia@cgtferroviario.es">ct.galicia@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>CASTILLA-LA MANCHA</TD><TD>Juan Carlos P&eacute;rez Aguilera</TD><TD><A HREF="mailto:ct.clm@cgtferroviario.es">ct.clm@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>EXTREMADURA</TD><TD>Pablo L&oacute;pez Zurdo</TD><TD><A HREF="mailto:ct.extremadura@cgtferroviario.es">ct.extremadura@cgtferroviario.es</A></TD>
+  </TR>
+  
+  
+  <TR><TD colspan=3><HR></TD></TR>
+
+
+  <TR>
+     <TD colspan=3 bgcolor=955E55><font color=white size=4><b>&nbsp;&nbsp;&nbsp; COORDINACI&Oacute;N ADIF</b></font></TD>
+  </TR>
+  <TR>
+     <TD>COORDINADOR</TD><TD>Enrique Villase&ntilde;or Osuna</TD><TD><A HREF="mailto:adif@cgtferroviario.es">adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>SALUD LABORAL</TD><TD>Antonio Terr&eacute; Jim&eacute;nez</TD><TD><A HREF="mailto:saludlaboral.adif@cgtferroviario.es">saludlaboral.adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>SEG. EN LA CIRCULACI&Oacute;N</TD><TD>Diego S&aacute;nchez &Aacute;lvarez</TD><TD><A HREF="mailto:seg.cir.adif@cgtferroviario.es">seg.cir.adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>POL&Iacute;TICA SOCIAL</TD><TD>Elena Garc&iacute;a Esparza</TD><TD><A HREF="mailto:politicasocial.adif@cgtferroviario.es">politicasocial.adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>FORMACI&Oacute;N</TD><TD>Amelia Penedo Fern&aacute;ndez</TD><TD><A HREF="mailto:formacion.adif@cgtferroviario.es">formacion.adif@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>INFRAESTRUCTURA</TD><TD>Carlos Montejano Andreu</TD><TD><A HREF="mailto:infraestructura@cgtferroviario.es">infraestructura@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>CIRCULACI&Oacute;N</TD><TD>Diego S&aacute;nchez &Aacute;lvarez</TD><TD><A HREF="mailto:circulacion@cgtferroviario.es">circulacion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>OFICINAS</TD><TD>Gonzalo Wilhelmi Casanova</TD><TD><A HREF="mailto:oficinas@cgtferroviario.es">oficinas@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>SERVICIOS LOG&Iacute;STICOS</TD><TD>Enrique Villase&ntilde;or Osuna</TD><TD><A HREF="mailto:servicioslogisticos@cgtferroviario.es">servicioslogisticos@cgtferroviario.es</A></TD>
+  </TR>
+  
+  
+  <TR><TD colspan=3><HR></TD></TR>
+   
+  
+  <TR>
+     <TD colspan=3 bgcolor=955E55><font color=white size=4><b>&nbsp;&nbsp;&nbsp; COORDINACI&Oacute;N RENFE</b></font></TD>
+  </TR>
+  <TR>
+     <TD>COORDINADOR</TD><TD>Pablo Rodr&iacute;guez Villanueva</TD><TD><A HREF="mailto:renfe@cgtferroviario.es">renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>SALUD LABORAL</TD><TD>Sabino Alberto Quesada</TD><TD><A HREF="mailto:saludlaboral.renfe@cgtferroviario.es">saludlaboral.renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>SEG. EN LA CIRCULACI&Oacute;N</TD><TD>Ignacio Barros Alonso</TD><TD><A HREF="mailto:seg.cir.renfe@cgtferroviario.es">seg.cir.renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>POL&Iacute;TICA SOCIAL</TD><TD>Rosario Portero C&oacute;rdoba</TD><TD><A HREF="mailto:politicasocial.renfe@cgtferroviario.es">politicasocial.renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>FORMACI&Oacute;N</TD><TD>Rosario Portero C&oacute;rdoba</TD><TD><A HREF="mailto:formacion.renfe@cgtferroviario.es">formacion.renfe@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>TRACCI&Oacute;N</TD><TD>Ignacio Barros Alonso</TD><TD><A HREF="mailto:traccion@cgtferroviario.es">traccion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>TALLERES</TD><TD>Pablo Rodr&iacute;guez Villanueva</TD><TD><A HREF="mailto:talleres@cgtferroviario.es">talleres@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>INTERVENCI&Oacute;N</TD><TD>Miguel Lirio</TD><TD><A HREF="mailto:intervencion@cgtferroviario.es">intervencion@cgtferroviario.es</A></TD>
+  </TR>
+  <TR>
+     <TD>COMERCIAL Y TAQUILLAS</TD><TD>Pablo Saeta Tejero</TD><TD><A HREF="mailto:comercial@cgtferroviario.es">cgtcomercialsff@gmail.com</A></TD>
+  </TR>
+  <TR>
+     <TD>OFICINAS</TD><TD>Sabino Alberto Quesada</TD><TD><A HREF="mailto:oficinas@cgtferroviario.es">oficinas@cgtferroviario.es</A></TD>
+  </TR>
+  <TR><TD colspan=3><HR></TD></TR>
+</TABLE>
+
+
+      </TD>
+      <TD><?php include "columna3.php"; ?></TD>
+    </TR>
+    <TR valign=top>
+      <TD colspan=3 style="text-align:center;"><?php include "enlaces.php"; ?></TD>
+    </TR>
+    <TR valign=top>
+      <TD colspan=3 style="text-align:center;"><?php include "pie.php"; ?></TD>
+    </TR>
+</TABLE>
+
+</BODY>
+</HTML>
